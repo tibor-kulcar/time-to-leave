053b0e5c2eaf5a8e1eaa1b93590804e52745111a
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-02-21 10:11:41 +0100

    use lowercase for search

--- components/StopSearch.jsx
+++ components/StopSearch.jsx
@@ -1,64 +1,62 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Autocomplete from 'react-native-autocomplete-input';
 import pidStops from '../external_data/pid-stops.json';
 
+const stopNames = pidStops.map(stop => stop.stop_name);
+// remove duplicates
+const stops = [...new Set(stopNames)];
+
 const StopSearch = ({setInput}) => {
-  const stops = pidStops.map(stop => stop.stop_name);
-  //console.log('props', props)
   const [text, setText] = useState('');
-  
+
   const filterData = (text) => {
     if (text == '') return [''];
-    const filteredStops = stops.filter(stop => stop.includes(text)); 
-    
-    if (text == filteredStops[0]) return[''];
 
+    const filteredStops = stops.filter(stop => stop.toLowerCase().includes(text.toLowerCase()));
+    if (text == filteredStops[0]) return[''];
     return filteredStops;
   };
 
   const data = filterData(text);
   return (
-    
-   // {text};
     <>
       <Autocomplete
         data={data}
         value={text}
+        style={styles.input}
         onChangeText={(txt) => {
-          setText(txt)
-          setInput(txt)
-
-        
-        console.log(txt)
-      }}
+          setText(txt);
+          setInput(txt);
+        }}
         flatListProps={{
           keyExtractor: (_, idx) => idx,
           renderItem: ({ item }) => (
-            <TouchableOpacity onPress={() => {
-              setInput(item)
-              setText(item)
-            }}>
-              <Text>{item}</Text>
+            <TouchableOpacity
+              onPress={() => {
+                setText(item);
+                setInput(item);
+              }}
+            >
+              <Text style={styles.text}>{item}</Text>
             </TouchableOpacity>
           ),
         }}
       />
-      <Text></Text>
     </>
   )
-
 };
 const styles = StyleSheet.create({
   text: {
+    padding: "5px",
     color: "#fff",
-    fontSize: "3rem", // Set the font size to 24
+    backgroundColor: "#000",
+    fontSize: "1rem", // Set the font size to 24
   },
   input: {
     height: 40,
-    color: "#fff",
-    margin: 12,
-    borderWidth: 1,
+    color: "#000",
+    borderWidth: 0,
     padding: 10,
   },
 });
