e5eac54c45566b2357ff4daa86539fa96f9f7bea
Martin Nabelek <nabelekm@gmail.com>
2023-02-21 02:44:17 +0100

    Stops Filtering - basic functionality

--- components/StopSearch.jsx
+++ components/StopSearch.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput } from 'react-native';
+import Autocomplete from 'react-native-autocomplete-input';
+import pidStops from '../external_data/pid-stops.json';
 
+const StopSearch = ({onChangeInput}) => {
+  const stops = pidStops.map(stop => stop.stop_name);
+  //console.log('props', props)
+  const [text, setText] = useState('');
 
-const StopSearch = () => {
-  const name = 'Stopsearch';
+  const filterData = (text) => {
+    if (text == '') return [''];
+    const filteredStops = stops.filter(stop => stop.includes(text)); 
+     return filteredStops;
+  };
+
+  const data = filterData(text);
   return (
+    
+   // {text};
     <>
-      <Text style={styles.text}>Hello, I am {name}!</Text>;
+      <Autocomplete
+        data={data}
+        value={text}
+        onChangeText={(txt) => {
+          setText(txt)
+        
+        console.log(txt)
+      }}
+        flatListProps={{
+          keyExtractor: (_, idx) => idx,
+          renderItem: ({ item }) => <Text>{item}</Text>,
+        }}
+      />
       <Text></Text>
     </>
   )
+
 };
 const styles = StyleSheet.create({
   text: {
     color: "#fff",
     fontSize: "3rem", // Set the font size to 24
-  }
+  },
+  input: {
+    height: 40,
+    color: "#fff",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 
