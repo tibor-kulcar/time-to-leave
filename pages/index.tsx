8eb496b0235ab7cc243c104fd7325dc47d7e14cb
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-03-24 00:58:17 +0100

    text ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="">
       <main className="flex">
-        <h1 className="text-3xl">Time to Leave</h1>
+        <h1 className="text-3xl">Time to Leave DUDE</h1>
       </main>
     </div>
   );
