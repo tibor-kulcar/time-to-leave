a73a5e14745074d65ae3dbed4149d1ce643fdd10
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-03-27 15:21:08 +0200

    set colors for react-select

--- components/StopSearch/StopSearch.styles.ts
+++ components/StopSearch/StopSearch.styles.ts
@@ -1,5 +1,5 @@
 export const controlStyles = {
-  base: 'border-b bg-gray-900 hover:cursor-pointer',
+  base: 'border-b bg-black hover:cursor-pointer',
   focus: 'border-white',
   nonFocus: 'border-gray-300 hover:border-gray-400',
 };
@@ -18,7 +18,7 @@ export const clearIndicatorStyles =
 export const indicatorSeparatorStyles = 'hidden bg-gray-300';
 export const dropdownIndicatorStyles =
   'w-0 h-0 p-1 hover:bg-gray-100 text-gray-500 rounded-md hover:text-black';
-export const menuStyles = 'z-50 py-1 mt-2 bg-black';
+export const menuStyles = 'z-50 py-1 mt-2 bg-gray-900';
 export const groupHeadingStyles = 'ml-3 mt-2 mb-1 text-gray-500 text-sm';
 export const optionStyles = {
   base: 'hover:cursor-pointer px-3 py-2 rounded',
