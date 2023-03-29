faf61bd9ad79dbbf2decb61e0922bce6faae5ba3
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-03-29 11:47:01 +0200

    optimize for PWA, accesibility

--- components/StopSearch/StopSearch.styles.ts
+++ components/StopSearch/StopSearch.styles.ts
@@ -19,11 +19,11 @@ export const indicatorSeparatorStyles = 'hidden bg-gray-300';
 export const dropdownIndicatorStyles =
   'w-0 h-0 p-1 hover:bg-gray-100 text-gray-500 rounded-md hover:text-black';
 export const menuStyles =
-  'z-50 pl-3 py-1 mt-3 text-3xl font-light bg-black/[.75] backdrop-blur-sm';
+  'z-50 pl-3 py-1 mt-3 text-3xl font-light bg-black/75 backdrop-blur-sm';
 export const groupHeadingStyles = 'ml-3 mt-2 mb-1 text-gray-500 text-3xl';
 export const optionStyles = {
-  base: 'py-2 text-3xl text-gray-500 leading-relaxed hover:cursor-pointer',
-  focus: 'text-white',
+  base: 'py-2 text-3xl text-white/[.8] leading-relaxed hover:cursor-pointer',
+  focus: 'text-white/[.99]',
   selected: "after:content-['●'] after:ml-2 after:text-gray-300 text-gray-500",
 };
 export const noOptionsMessageStyles = 'text-3xl leading-loose text-gray-500';
