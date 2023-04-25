87ee584e047c2fbb8cdf306a3e34945a8c8d60bb
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-04-25 22:23:15 +0200

    sort platforms alphabetically

--- hooks/useGroupDepartures.ts
+++ hooks/useGroupDepartures.ts
@@ -1,22 +1,22 @@
 import { DepartureProps, GroupedDepartureProps } from '../types';
 
 export function useGroupDepartures(departures: DepartureProps[]) {
-  const groupedData = departures?.reduce(
-    (result: GroupedDepartureProps[], current: DepartureProps) => {
-      const platformCode = current.stop.platform_code;
-      const platformIndex = result.findIndex(
-        (platform) => platform.platformCode === platformCode
-      );
+  const groupedData: GroupedDepartureProps[] = [];
 
-      if (platformIndex === -1) {
-        result.push({ platformCode: platformCode, departures: [current] });
-      } else {
-        result[platformIndex].departures.push(current);
-      }
-      return result;
-    },
-    []
+  departures?.forEach((departure) => {
+    const platformCode = departure.stop.platform_code;
+    const platformIndex = groupedData.findIndex(
+      (group) => group.platformCode === platformCode
+    );
+
+    if (platformIndex === -1) {
+      groupedData.push({ platformCode, departures: [departure] });
+    } else {
+      groupedData[platformIndex].departures.push(departure);
+    }
+  });
+
+  return groupedData.sort((a, b) =>
+    a.platformCode?.localeCompare(b.platformCode)
   );
-  // console.log(groupedData);
-  return groupedData;
 }
