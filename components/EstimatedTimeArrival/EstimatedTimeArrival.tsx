97d41bdbf4feb46a398e6e90f71069705b61f9a3
Tibor Kulčár <kulcar.tibor@gmail.com>
2023-04-26 00:02:08 +0200

    refactor eta calculation, add support for hours

--- components/EstimatedTimeArrival/EstimatedTimeArrival.tsx
+++ components/EstimatedTimeArrival/EstimatedTimeArrival.tsx
@@ -2,17 +2,18 @@ type EstimatedTimeArrivalProps = {
   diff: number;
 };
 
-const twoDigits = new Intl.NumberFormat('en-US', {
-  style: 'decimal',
-  minimumIntegerDigits: 2,
-});
+const formatTimeUnit = (unit: number) => (unit | 0).toString().padStart(2, '0');
+
+const TIME_UNITS = [3600000, 60000, 1000];
 
 const getTimeDiff = (diff: number) => {
-  const sec = diff / 1000;
-  const min = Math.floor(sec / 60);
-  const diffSecs = Math.floor(sec - min * 60);
+  const timeParts = TIME_UNITS.map((unit, i) => {
+    const timeValue = ((diff / unit) | 0) % 60;
+    if (i === 0 && timeValue === 0) return null; // Skip hours if 0
+    return `${formatTimeUnit(timeValue)}`;
+  }).filter(Boolean);
 
-  return twoDigits.format(min) + ':' + twoDigits.format(diffSecs);
+  return timeParts.join(':');
 };
 
 const EstimatedTimeArrival = ({ diff }: EstimatedTimeArrivalProps) => {
