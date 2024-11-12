3630b51290fd21b5b2c6f69de0741a473cbf5511
Martin Nabelek <nabelekm@gmail.com>
2024-11-12 22:51:54 +0100

    feat: notification test

--- components/DepartureItem/DepartureItem.tsx
+++ components/DepartureItem/DepartureItem.tsx
@@ -1,7 +1,10 @@
+'use client';
 import clsx from 'clsx';
 
 import { DepartureProps } from '@/types';
 import { EstimatedTimeArrival } from '@/components/EstimatedTimeArrival';
+import { useNotification } from '@/notifications/useNotification';
+import { useState } from 'react';
 
 // const walkingTimeInMilisecs = 3 * 60 * 1000;
 
@@ -12,7 +15,18 @@ type DepartureItemProps = {
 
 const DepartureItem = ({ departure, time }: DepartureItemProps) => {
   // console.count('DepartureItem');
+  const { isSupported, isSubscribed, handleSubscribe, subscription } =
+    useNotification();
 
+  const sendNotification = async (title: string, message: string) => {
+    await fetch('/api/web-push/send', {
+      method: 'POST',
+      body: JSON.stringify({ title, message, subscription }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  };
   const prediction = new Date(
     departure.departure_timestamp.predicted ||
       departure.departure_timestamp.scheduled
@@ -23,8 +37,9 @@ const DepartureItem = ({ departure, time }: DepartureItemProps) => {
 
   return (
     <div
+      onClick={() => sendNotification('title', 'message')}
       className={clsx(
-        'grid grid-flow-col gap-2 auto-cols-[_2.8rem_2fr_auto] w-full text-2xl leading-tight'
+        'gap-2 grid grid-flow-col auto-cols-[_2.8rem_2fr_auto] w-full text-2xl leading-tight'
         // diff < walkingTimeInMilisecs ? 'text-bone-600 dark:text-bone-800' : ''
       )}
     >
