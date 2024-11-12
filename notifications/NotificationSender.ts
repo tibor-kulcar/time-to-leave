2d4f18b3fed927b19f66a11f688375c71eddfe2a
Martin Nabelek <nabelekm@gmail.com>
2024-11-12 22:18:50 +0100

    fix: es compatibility

--- notifications/NotificationSender.ts
+++ notifications/NotificationSender.ts
@@ -1,9 +1,9 @@
 import webpush, { PushSubscription } from 'web-push';
 
 webpush.setVapidDetails(
-  'mailto:' + process.env.VAPID_EMAIL ?? '',
-  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? '',
-  process.env.VAPID_PRIVATE_KEY ?? ''
+  'mailto:' + process.env.VAPID_EMAIL,
+  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '',
+  process.env.VAPID_PRIVATE_KEY || ''
 );
 
 export const sendNotification = async (
